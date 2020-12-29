8b027ecc7aeb3d2b1860ff0695dab9bcb87fb370
bever209 <74867461+bever209@users.noreply.github.com>
2020-12-29 14:57:28 -0500

    Update RemoveBadFiles.sh

--- Linux-Ubuntu/RemoveBadFiles.sh
+++ Linux-Ubuntu/RemoveBadFiles.sh
@@ -1,87 +1,87 @@
 #!/bin/bash
 
-echo "Running remove bad files program"
+echo "Running remove bad files program (specifically media files)"
 
-find / -name "*.midi" -type f >> ~/Desktop/Script.log
-find / -name "*.mid" -type f >> ~/Desktop/Script.log
-find / -name "*.mod" -type f >> ~/Desktop/Script.log
-find / -name "*.mp3" -type f >> ~/Desktop/Script.log
-find / -name "*.mp2" -type f >> ~/Desktop/Script.log
-find / -name "*.mpa" -type f >> ~/Desktop/Script.log
-find / -name "*.abs" -type f >> ~/Desktop/Script.log
-find / -name "*.mpega" -type f >> ~/Desktop/Script.log
-find / -name "*.au" -type f >> ~/Desktop/Script.log
-find / -name "*.snd" -type f >> ~/Desktop/Script.log
-find / -name "*.wav" -type f >> ~/Desktop/Script.log
-find / -name "*.aiff" -type f >> ~/Desktop/Script.log
-find / -name "*.aif" -type f >> ~/Desktop/Script.log
-find / -name "*.sid" -type f >> ~/Desktop/Script.log
-find / -name "*.flac" -type f >> ~/Desktop/Script.log
-find / -name "*.ogg" -type f >> ~/Desktop/Script.log
+find / -name "*.midi" -type f -delete
+find / -name "*.mid" -type f -delete
+find / -name "*.mod" -type f -delete
+find / -name "*.mp3" -type f -delete
+find / -name "*.mp2" -type f -delete
+find / -name "*.mpa" -type f -delete
+find / -name "*.abs" -type f -delete
+find / -name "*.mpega" -type f -delete
+find / -name "*.au" -type f -delete
+find / -name "*.snd" -type f -delete
+find / -name "*.wav" -type f -delete
+find / -name "*.aiff" -type f -delete
+find / -name "*.aif" -type f -delete
+find / -name "*.sid" -type f -delete
+find / -name "*.flac" -type f -delete
+find / -name "*.ogg" -type f -delete
 
-find / -name "*.mpeg" -type f >> ~/Desktop/Script.log
-find / -name "*.mpg" -type f >> ~/Desktop/Script.log
-find / -name "*.mpe" -type f >> ~/Desktop/Script.log
-find / -name "*.dl" -type f >> ~/Desktop/Script.log
-find / -name "*.movie" -type f >> ~/Desktop/Script.log
-find / -name "*.movi" -type f >> ~/Desktop/Script.log
-find / -name "*.mv" -type f >> ~/Desktop/Script.log
-find / -name "*.iff" -type f >> ~/Desktop/Script.log
-find / -name "*.anim5" -type f >> ~/Desktop/Script.log
-find / -name "*.anim3" -type f >> ~/Desktop/Script.log
-find / -name "*.anim7" -type f >> ~/Desktop/Script.log
-find / -name "*.avi" -type f >> ~/Desktop/Script.log
-find / -name "*.vfw" -type f >> ~/Desktop/Script.log
-find / -name "*.avx" -type f >> ~/Desktop/Script.log
-find / -name "*.fli" -type f >> ~/Desktop/Script.log
-find / -name "*.flc" -type f >> ~/Desktop/Script.log
-find / -name "*.mov" -type f >> ~/Desktop/Script.log
-find / -name "*.qt" -type f >> ~/Desktop/Script.log
-find / -name "*.spl" -type f >> ~/Desktop/Script.log
-find / -name "*.swf" -type f >> ~/Desktop/Script.log
-find / -name "*.dcr" -type f >> ~/Desktop/Script.log
-find / -name "*.dir" -type f >> ~/Desktop/Script.log
-find / -name "*.dxr" -type f >> ~/Desktop/Script.log
-find / -name "*.rpm" -type f >> ~/Desktop/Script.log
-find / -name "*.rm" -type f >> ~/Desktop/Script.log
-find / -name "*.smi" -type f >> ~/Desktop/Script.log
-find / -name "*.ra" -type f >> ~/Desktop/Script.log
-find / -name "*.ram" -type f >> ~/Desktop/Script.log
-find / -name "*.rv" -type f >> ~/Desktop/Script.log
-find / -name "*.wmv" -type f >> ~/Desktop/Script.log
-find / -name "*.asf" -type f >> ~/Desktop/Script.log
-find / -name "*.asx" -type f >> ~/Desktop/Script.log
-find / -name "*.wma" -type f >> ~/Desktop/Script.log
-find / -name "*.wax" -type f >> ~/Desktop/Script.log
-find / -name "*.wmv" -type f >> ~/Desktop/Script.log
-find / -name "*.wmx" -type f >> ~/Desktop/Script.log
-find / -name "*.3gp" -type f >> ~/Desktop/Script.log
-find / -name "*.mov" -type f >> ~/Desktop/Script.log
-find / -name "*.mp4" -type f >> ~/Desktop/Script.log
-find / -name "*.avi" -type f >> ~/Desktop/Script.log
-find / -name "*.swf" -type f >> ~/Desktop/Script.log
-find / -name "*.flv" -type f >> ~/Desktop/Script.log
-find / -name "*.m4v" -type f >> ~/Desktop/Script.log
+find / -name "*.mpeg" -type f -delete
+find / -name "*.mpg" -type f -delete
+find / -name "*.mpe" -type f -delete
+find / -name "*.dl" -type f -delete
+find / -name "*.movie" -type f -delete
+find / -name "*.movi" -type f -delete
+find / -name "*.mv" -type f -delete
+find / -name "*.iff" -type f -delete
+find / -name "*.anim5" -type f -delete
+find / -name "*.anim3" -type f -delete
+find / -name "*.anim7" -type f -delete
+find / -name "*.avi" -type f -delete
+find / -name "*.vfw" -type f -delete
+find / -name "*.avx" -type f -delete
+find / -name "*.fli" -type f -delete
+find / -name "*.flc" -type f -delete
+find / -name "*.mov" -type f -delete
+find / -name "*.qt" -type f -delete
+find / -name "*.spl" -type f -delete
+find / -name "*.swf" -type f -delete
+find / -name "*.dcr" -type f -delete
+find / -name "*.dir" -type f -delete
+find / -name "*.dxr" -type f -delete
+find / -name "*.rpm" -type f -delete
+find / -name "*.rm" -type f -delete
+find / -name "*.smi" -type f -delete
+find / -name "*.ra" -type f -delete
+find / -name "*.ram" -type f -delete
+find / -name "*.rv" -type f -delete
+find / -name "*.wmv" -type f -delete
+find / -name "*.asf" -type f -delete
+find / -name "*.asx" -type f -delete
+find / -name "*.wma" -type f -delete
+find / -name "*.wax" -type f -delete
+find / -name "*.wmv" -type f -delete
+find / -name "*.wmx" -type f -delete
+find / -name "*.3gp" -type f -delete
+find / -name "*.mov" -type f -delete
+find / -name "*.mp4" -type f -delete
+find / -name "*.avi" -type f -delete
+find / -name "*.swf" -type f -delete
+find / -name "*.flv" -type f -delete
+find / -name "*.m4v" -type f -delete
 
-find / -name "*.tiff" -type f >> ~/Desktop/Script.log
-find / -name "*.tif" -type f >> ~/Desktop/Script.log
-find / -name "*.rs" -type f >> ~/Desktop/Script.log
-find / -name "*.im1" -type f >> ~/Desktop/Script.log
-find / -name "*.gif" -type f >> ~/Desktop/Script.log
-find / -name "*.jpeg" -type f >> ~/Desktop/Script.log
-find / -name "*.jpg" -type f >> ~/Desktop/Script.log
-find / -name "*.jpe" -type f >> ~/Desktop/Script.log
-find / -name "*.png" -type f >> ~/Desktop/Script.log
-find / -name "*.rgb" -type f >> ~/Desktop/Script.log
-find / -name "*.xwd" -type f >> ~/Desktop/Script.log
-find / -name "*.xpm" -type f >> ~/Desktop/Script.log
-find / -name "*.ppm" -type f >> ~/Desktop/Script.log
-find / -name "*.pbm" -type f >> ~/Desktop/Script.log
-find / -name "*.pgm" -type f >> ~/Desktop/Script.log
-find / -name "*.pcx" -type f >> ~/Desktop/Script.log
-find / -name "*.ico" -type f >> ~/Desktop/Script.log
-find / -name "*.svg" -type f >> ~/Desktop/Script.log
-find / -name "*.svgz" -type f >> ~/Desktop/Script.log
+find / -name "*.tiff" -type f -delete
+find / -name "*.tif" -type f -delete
+find / -name "*.rs" -type f -delete
+find / -name "*.im1" -type f -delete
+find / -name "*.gif" -type f -delete
+find / -name "*.jpeg" -type f -delete
+find / -name "*.jpg" -type f -delete
+find / -name "*.jpe" -type f -delete
+find / -name "*.png" -type f -delete
+find / -name "*.rgb" -type f -delete
+find / -name "*.xwd" -type f -delete
+find / -name "*.xpm" -type f -delete
+find / -name "*.ppm" -type f -delete
+find / -name "*.pbm" -type f -delete
+find / -name "*.pgm" -type f -delete
+find / -name "*.pcx" -type f -delete
+find / -name "*.ico" -type f -delete
+find / -name "*.svg" -type f -delete
+find / -name "*.svgz" -type f -delete
 
 find / -name '*.mp3' -type f -delete
 find / -name '*.mov' -type f -delete
@@ -93,9 +93,9 @@ find / -name '*.flac' -type f -delete
 find / -name '*.m4a' -type f -delete
 find / -name '*.flv' -type f -delete
 find / -name '*.ogg' -type f -delete
-find /home -name '*.gif' -type f -delete
-find /home -name '*.png' -type f -delete
-find /home -name '*.jpg' -type f -delete
-find /home -name '*.jpeg' -type f -delete
+find / -name '*.gif' -type f -delete
+find / -name '*.png' -type f -delete
+find / -name '*.jpg' -type f -delete
+find / -name '*.jpeg' -type f -delete
   
   
