40e432b1f07c9299657ae98a44cd836f7ac4302e
bever209 <74867461+bever209@users.noreply.github.com>
2021-01-01 21:01:15 -0500

    Update Users.sh

--- Linux-Ubuntu/Users.sh
+++ Linux-Ubuntu/Users.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-# DO THIS MANUALLY IN THE COMP
+#Lock people from logging straight into the root account
+passwd -l root
+echo "Finished locking the root account"
+
+#Change the ownership and permissions of files that could commonly be exploited otherwise
+chown root:root /etc/securetty
+chmod 0600 /etc/securetty
+chmod 644 /etc/crontab
+chmod 640 /etc/ftpusers
+chmod 440 /etc/inetd.conf
+chmod 440 /etc/xinetd.conf
+chmod 400 /etc/inetd.d
+chmod 644 /etc/hosts.allow
+chmod 440 /etc/sudoers
+chmod 640 /etc/shadow
+chown root:root /etc/shadow
+echo "Finished changing permissions"
+
+echo "Done"
+clear
